5dfcd476a996b036bff75e158a66e0b2875a82f6
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-03-30 18:19:08 +0200

    Default settings file added

---
+++ settings-default.py
@@ -0,0 +1,6 @@
+token = 'some-dataverse-token-like-3995-42b3-9861-ec81a26015ef'
+host = "some-dataverse-host"
+newmap = './mappings/mappings.csv'
+humap = './mappings/humapping.csv'
+url = "http://www.some-url/cgi-bin/katalogus/tarkiser_en.pl"
+PUBLISH = 1
